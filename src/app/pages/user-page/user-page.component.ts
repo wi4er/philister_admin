86e76d8e5d51467efd4d5d2376acdb9217ab0ad0
Maxim Loboda <wi4er777@gmail.com>
2023-01-07 07:53:32 +0200

    user page navigation

--- src/app/pages/user-page/user-page.component.ts
+++ src/app/pages/user-page/user-page.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from "@angular/router";
+import { MatTabChangeEvent } from "@angular/material/tabs";
+import { FormControl } from "@angular/forms";
 
 @Component({
   selector: 'app-user-page',
   templateUrl: './user-page.component.html',
-  styleUrls: ['./user-page.component.css']
+  styleUrls: [ './user-page.component.css' ]
 })
 export class UserPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+  ) {
+  }
+
+  pages: { [key: number]: string } = {
+    0: 'user',
+    1: 'group',
+    2: 'contact'
+  }
+
+  selected = new FormControl(0);
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe(value => {
+      for (const key in this.pages) {
+        if (this.pages[key] === value['page']) {
+          this.selected.setValue(+key);
+        }
+      }
+    });
+  }
+
+  handleChange(index: number) {
+    this.router.navigate(
+      [ '/user' ],
+      { queryParams: { page: this.pages[index] } }
+    );
   }
 
 }
